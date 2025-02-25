76d4e8de39e8a7b27e2389f1ec2f3db479ee72cf
Sahil-tried-coding <sahiltriedcoding@gmail.com>
2025-02-25 10:46:55 -0500

    fixxed some error

--- src/Dashboard/components/resume/component/forms/Skills.jsx
+++ src/Dashboard/components/resume/component/forms/Skills.jsx
@@ -20,7 +20,7 @@ function Skills() {
   }]);
 
 
-  console.log("this is the rating here 💕💕💕💕",skillsList?.rating)
+  console.log("ok ok ok 💕💕💕💕",skillsList?.rating)
   useEffect(() => {
     if (resumeInfo?.skills && Array.isArray(resumeInfo.skills) && resumeInfo.skills.length > 0) {
       setSkillsList(resumeInfo.skills);
